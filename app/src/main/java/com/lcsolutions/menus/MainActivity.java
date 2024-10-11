afe33494a8e3a01e3eb1827095fd7eccf513f9c1
Luis <luisangel.cruz@upaep.edu.mx>
2024-10-11 11:21:11 -0600

    v1

--- app/src/main/java/com/lcsolutions/menus/MainActivity.java
+++ app/src/main/java/com/lcsolutions/menus/MainActivity.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -25,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         edtContext = findViewById(R.id.edtContext);
 
         registerForContextMenu(edtMenu);
+        registerForContextMenu(edtContext);
 
 
     }
@@ -39,13 +43,59 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-
         super.onCreateContextMenu(menu, v, menuInfo);
-
-        menu.setHeaderTitle("Contextual");
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.my_option_menu, menu);
+        if (v == edtMenu){
+            menu.setHeaderTitle("Contextual");
+            menuInflater.inflate(R.menu.my_option_menu, menu);
+        } else if (v == edtContext) {
+            menu.setHeaderTitle("Editmenu ");
+            menuInflater.inflate(R.menu.my_option_menu,menu);
+        }
+
+        //menuInflater.inflate(R.menu.my_option_menu, menu);
+
+        //MenuInflater menuInflater = getMenuInflater();
+
 
 
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if(item.getItemId() == R.id.mnuOpcion1){
+            edtContext.setText("Se presiono la opcion uno del menu");
+        }
+        if(item.getItemId() == R.id.mnuOpcion2){
+            edtContext.setText("Se presiono la opcion dos del menu");
+        }
+        if(item.getItemId() == R.id.mnuOpcion3){
+            edtContext.setText("Se presiono la opcion tres del menu");
+        }
+
+        return true;
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        super.onContextItemSelected(item);
+        if(item.getItemId()== R.id.mnuItem1){
+            edtContext.setText("Se presiono el menu opcion 1");
+        }
+        if(item.getItemId()== R.id.mnuItem2){
+            edtContext.setText("Se presiono el menu opcion 2");
+        }
+        if(item.getItemId()== R.id.mnuItem4){
+            edtContext.setText("Se presiono el menu opcion3");
+        }
+        if(item.getItemId() == R.id.item_copiar){
+            Toast.makeText(this, "Se presiono copiar", Toast.LENGTH_SHORT).show();
+        }
+
+        return true;
+    }
+
 }
